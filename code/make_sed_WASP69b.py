e382f8c3c3d43d3ce4aef0deb28d6ed83ee81cdd
pcubillos <patricio.cubillos@oeaw.ac.at>
2026-07-29 16:46:20 +0200

    Updated WASP-69 SED code and file

--- code/make_sed_WASP69b.py
+++ code/make_sed_WASP69b.py
@@ -1,24 +1,17 @@
-# pip install stsynphot
-import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 import pyratbay.spectrum as ps
 import pyratbay.constants as pc
 import pyratbay.io as io
-import os
 from astropy import units as u
 from stsynphot.catalog import grid_to_spec
+import gen_tso.pandeia_io as pandeia
 
 
-def remake_from_helios():
+def make_sed():
     """
-    synphot is equivalent to pysynphot (which I originally used 
-    to create my PHOENIX SEDs)
+    Here are three other ways to generate SEDs
     """
-    resolution = 750.0
-    bin_wl = ps.constant_resolution_spectrum(0.1, 50.0, resolution)
-
-    # Synphot models
+    # OPTION 1: Synphot models
     teff = 4700.0
     log_gstar = 4.5
     Fe_H_star = 0.15
@@ -26,49 +19,68 @@ def remake_from_helios():
     sp = grid_to_spec('phoenix', teff, Fe_H_star, log_gstar)
     sp_wl = sp.waveset.to('micron').value
     sp_flux = sp(sp.waveset, flux_unit=u.mJy).value * 1e-26 * pc.c
+    resolution = 750.0
+    bin_wl = ps.constant_resolution_spectrum(0.1, 50.0, resolution)
     bin_flux = ps.bin_spectrum(bin_wl, sp_wl, sp_flux)
     io.write_spectrum(bin_wl, bin_flux, filename, 'emission')
 
-
-
-def plot_SEDs():
-    # System parameters
-    teff = 4700.0
-    log_gstar = 4.5
-    resolution = 150.0
-    bin_wl = ps.constant_resolution_spectrum(0.1, 50.0, resolution)
-    nbin = len(bin_wl)
-
-    filename = f'inputs/synphot_phoenix_WASP69b_{teff:.0f}K_m00_logg4.5.dat'
-    wl, flux = io.read_spectrum(filename, wn=False)
-    s_sed = ps.bin_spectrum(bin_wl, wl, flux)
-
-    flux, wn, t, g = ps.read_kurucz('inputs/fp02k2odfnew.pck', teff, log_gstar)
-    wl = 1.0 / wn / pc.um
-    k_sed = ps.bin_spectrum(bin_wl, wl, flux, gaps='ignore')
-
-    # The plot
-    fs = 11
-    colors = ['salmon', 'xkcd:green', 'royalblue']
-    fig = plt.figure(4)
-    plt.clf()
-    fig.set_size_inches(5.0, 3.15)
-    plt.subplots_adjust(0.11, 0.13, 0.995, 0.995)
-    ax = plt.subplot(111)
-    ax.plot(bin_wl, k_sed, c='royalblue', label='Kurucz')
-    ax.plot(bin_wl, s_sed, c='salmon', label='synphot', alpha=0.95)
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    ax.xaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
-    ax.xaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter())
-    ax.set_xticks([0.3, 1.0, 3.0, 10, 30])
-    ax.set_xlim(0.15, 30.0)
-    ax.set_ylim(1e0, 3e6)
-    ax.set_ylabel(
-        'WASP-69 SED (erg s$^{-1}$ cm$^{-2}$ cm)',
-        fontsize=fs, labelpad=0,
+    # OPTION 2: Use Pandeia package to get a PHOENIX SED model
+    # Closest SED to WASP-69 is an K2V model (Teff=4750K, logg=4.5)
+    scene = pandeia.make_scene(
+        sed_type='phoenix',
+        sed_model='k2v',
     )
-    ax.set_xlabel(r'wavelength ($\mathrm{\mu}$m)', fontsize=fs)
-    ax.legend(loc='lower right', fontsize=fs)
-    ax.tick_params(which='both', direction='in', labelsize=fs-1)
-    plt.savefig('plots/WASP69_sed.png', dpi=300)
+    sed_wl, flux = pandeia.extract_sed(scene, wl_range=(0.35,12.0))
+    # Convert flux from mJy to erg s-1 cm-2 cm-1
+    sed_flux = flux * pc.c / 1e26
+    bin_sed_flux = ps.bin_spectrum(bin_wl, sed_wl, sed_flux, gaps='interpolate')
+    starspec_file = 'inputs/phoenix_WASP69_K2V_4750K.dat'
+    io.write_spectrum(
+        bin_wl,
+        bin_sed_flux,
+        starspec_file,
+        type='emission',
+    )
+
+    # OPTION 3: Fetch a PHOENIX New ERA model
+    teff = 4700
+    logg = 4.5
+    metal = 0.15
+    folder = 'inputs/'
+    ps.fetch_phoenix(teff, logg, metal, folder)
+
+
+def compare_seds():
+    """
+    These are all PHOENIX models
+    """
+    # Synphot model
+    filename = 'inputs/synphot_phoenix_WASP69b_4700K_m00_logg4.5.dat'
+    wl, flux = io.read_spectrum(filename, False)
+
+    # New Era model
+    p_file = 'inputs/lte04700-4.50-0.0.PHOENIX-NewEra-ACES-COND-2023.HSR.h5'
+    p_wl, p_sed = ps.read_phoenix(p_file)
+    bin_new_era = ps.bin_spectrum(wl, p_wl, p_sed)
+
+    # Through Pandeia
+    starspec_file = 'inputs/phoenix_WASP69_K2V_4750K.dat'
+    gt_wl, gt_sed = io.read_spectrum(starspec_file, False)
+
+    # Take a look
+    plt.figure(0, (7., 4.))
+    plt.clf()
+    plt.subplots_adjust(0.07, 0.11, 0.98, 0.95)
+    ax = plt.subplot(111)
+    ax.plot(gt_wl, gt_sed, color='tomato', alpha=0.85, label='pandeia')
+    ax.plot(wl, flux, color='royalblue', alpha=0.85, label='synphot')
+    ax.plot(wl, bin_new_era, color='xkcd:green', alpha=0.85, label='new era')
+    ax.set_title('WASP-69 SED Spectrum')
+    ax.set_xscale('log')
+    ax.set_xlim(0.35, 10.5)
+    ax.set_xlabel(r'Wavelength ($\mathrm{\mu}$m)', fontsize=12)
+    ax.set_ylabel(r'$F_{\rm p}$ (erg s$^{-1}$ cm$^{-2}$ cm)', fontsize=12)
+    ax.tick_params(direction='in', which='both', labelsize=11)
+    ax.legend(loc='best')
+
+
